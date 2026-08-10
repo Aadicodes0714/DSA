40406bf900e8d73b913e0029a7ef5eca1c054fb6
Aditya Gupta <adityagupta7217@gmail.com>
2026-08-10 20:27:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (26.71%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.cpp
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -11,6 +11,8 @@
  */
 class Solution {
 public:
+
+/* // DFS TRAVERSAL ....
 TreeNode* dfs(TreeNode* root)
         {
             if (root==NULL)
@@ -27,6 +29,38 @@ if (root==NULL) return root;
 dfs(root);
 return root;
 
-
     }
+
+    */
+
+
+// BFS TRAVERSAL ...
+
+void bfs (TreeNode* root){
+    queue<TreeNode*>Q;
+    Q.push(root);
+
+    while(!Q.empty()){
+        TreeNode* cur=Q.front();    //nikaloo
+        Q.pop();
+        TreeNode* temp = cur->left;  //visit nhi.. swapping kro 
+        cur->left=cur->right;
+        cur->right=temp;   
+
+
+        if(cur->left!=NULL)          //push ->child
+                Q.push(cur->left);
+        if(cur->right!=NULL)
+                Q.push(cur->right);
+    }
+    }
+
+ TreeNode* invertTree(TreeNode* root){
+
+if(root==NULL)
+    return root;
+bfs(root);
+return root;
+
+ }
 };
