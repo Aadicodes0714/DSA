59042c4e8d939d85b9ce711a987fba67614dc824
Aditya Gupta <adityagupta7217@gmail.com>
2026-08-31 10:40:18 +0530

    Time: 0 ms (100.00%) | Memory: 24.6 MB (27.94%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -12,9 +12,9 @@
 class Solution {
 public:
 
-    void inorder(TreeNode* root, vector<int>& ans) {
+     void inorder(TreeNode* root, vector<int>& ans) {
         if (root == NULL)
-            return;
+            return ;
 
         inorder(root->left, ans);
         ans.push_back(root->val);
