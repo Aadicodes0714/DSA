7a691798308d50d7c0ee2d884aa2779a996f0fbc
Aditya Gupta <adityagupta7217@gmail.com>
2026-09-10 05:54:19 +0530

    Time: 12 ms (6.11%) | Memory: 263.4 MB (30.44%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,9 +1,10 @@
 class Solution {
+    /*
 public:
     void rotate(vector<int>& nums, int k) {
 
         //SIMPLE APPROACH....
-/*
+
         for(int j = 0; j < k; j++)
 {
     int last = nums[nums.size()-1];
@@ -17,8 +18,9 @@ public:
 }
 */
 
+/*
+//REVERSE APPROACH--> using STL libraries....
 
-//REVERSE APPROACH...
         int n=nums.size();
 
 k = k% n;
@@ -26,6 +28,36 @@ k = k% n;
 reverse(nums.begin(),nums.end());
 reverse(nums.begin(),nums.begin()+k);
 reverse(nums.begin()+k,nums.end());
+
+ }
+};
+
+*/
+
+// REVERSE APPROACH --> WITHOUT USING STL libraries...
+
+
+public:
+
+    void reverse ( vector<int>&nums,int left ,int right){
+  while(left<right){
+    int temp=nums[left];
+    nums[left]=nums[right];
+    nums[right]=temp;
+    left++;
+    right--;
+    }
+
+}
+    void rotate(vector<int>& nums, int k) {
+
+        int n=nums.size();
+
+k = k% n;
+
+reverse(nums,0,n-1);
+reverse(nums,0,k-1);
+reverse(nums, k, n-1);
 
 
     }
