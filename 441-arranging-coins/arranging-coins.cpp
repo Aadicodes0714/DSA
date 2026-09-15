102edd111d420fa13ac50c8bf30f9f6070b3b844
Aditya Gupta <adityagupta7217@gmail.com>
2026-09-15 19:47:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (67.81%) - LeetSync

--- 441-arranging-coins/arranging-coins.cpp
+++ 441-arranging-coins/arranging-coins.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-
+/*
 //BRUTEFORCE .....T.C- O(under root n).... S.C- O(1)
-
 int row=1;
-
 while(row<=n)
 {
     n=n-row;
     row++;
-
 }
-
 return row-1;
-
-
-
-
-
+*/
 //BINARY SEARCH...[OPTIMAL]... T.C- O(log n)... S.C- O(1)
 
+long long low=0;
+long long high=n-1;
+while(low<=high)
+{
+    long long mid=low+(high-low)/2;
+    if((mid*(mid+1)/2) == n)
+    {
+        return mid;
+    }
+     if((mid*(mid+1)/2) < n)
+    {
+        low=mid+1;
+
+    }
+     if((mid*(mid+1)/2) > n)
+    {
+        high=mid-1;
+    }
+
+    if (n==1)
+    {
+        return 1;
+    }
+}
+return high;
     }
 };
