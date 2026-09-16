2344ec7f4fb8fc0634c6e1e1c6b4eced62fabf5b
Aditya Gupta <adityagupta7217@gmail.com>
2026-09-17 00:48:06 +0530

    Time: 8 ms (24.25%) | Memory: 30.3 MB (66.46%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-               //brute forcle solution 
+        int n=nums.size();
+for(int i=0;i<n;i++)
+{
+    nums[i]*=nums[i];
+}
 
-      for (int i=0; i<nums.size(); i++)  //O(n)
-      {
-        nums[i]=nums[i]*nums[i];
+sort(nums.begin(),nums.end());
 
-      }
-      sort(nums.begin(),nums.end()); //O(nlogn)
-      return nums;
-     
+return nums;
+
     }
 };
